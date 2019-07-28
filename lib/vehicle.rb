760e8b80be424bf1b0d81907503ba321b2e30f6c
Amy Fogelman <abfnj1@gmail.com>
2019-07-28 20:40:24 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,14 @@
 class Vehicle
 
+
+  attr_accessor
+:wheel_size,  
+:wheel_number
+
+  def
+initalize (wheel_size, wheel_number)
+  @wheel_size = wheel_size
+  @wheel_number = wheel_number
+
+
 end
